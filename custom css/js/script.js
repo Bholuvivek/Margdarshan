794d962039d59dd301e352d94fcff48cc8b54671
Vivek Kumar <102277762+Bholuvivek@users.noreply.github.com>
2024-02-21 11:31:09 +0530

    Commit

---
+++ custom css/js/script.js
@@ -0,0 +1,38 @@
+//  Welcom 
+
+const forphoto = document.querySelector('.forphoto')
+
+forphoto.innerHTML = '<img src="./img/photo.svg" class="photo1" alt="" />'
+
+//  Carda Section 
+
+
+// Card 1
+const session1 = document.querySelector('.sessions1')
+session1.innerHTML = '<span>4</span>'
+
+const planame1 = document.querySelector('.planname1')
+
+planame1.innerHTML = 'Swift Assist'
+const plan1 = document.querySelector('.plan1')
+
+plan1.innerHTML = '₹1500'
+
+
+
+
+
+
+
+
+const plan2 = document.querySelector('.plan2')
+
+plan2.innerHTML = '₹3600'
+
+const plan3 = document.querySelector('.plan3')
+
+plan3.innerHTML = '₹4750'
+
+const plan4 = document.querySelector('.plan4')
+
+plan4.innerHTML = '₹8100'
